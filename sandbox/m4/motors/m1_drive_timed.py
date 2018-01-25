7f2f27f990d585633e12071a997fb57159df252a
townsetw <townsetw@rose-hulman.edu>
2018-01-25 15:10:58 -0500

    Working on drive motors

--- sandbox/m4/motors/m1_drive_timed.py
+++ sandbox/m4/motors/m1_drive_timed.py
@@ -89,11 +89,12 @@ def main():
 
     time_s = 1  # Any value other than 0.
     while time_s != 0:
-        left_sp = int(input("Enter a speed for the left motor (0 to 900 dps): "))
-        right_sp = int(input("Enter a speed for the right motor (0 to 900 dps): "))
-        time_s = int(input("Enter a time to drive (seconds): "))
+        left_sp = int(input("Enter a speed for the motor (0 to 900 dps): "))
+        right_sp = left_sp
+        distance_in = int(input("Enter a distance to travel (inches): "))
         left_motor.run_forever(speed_sp=left_sp)
         right_motor.run_forever(speed_sp=right_sp)
+        time_s = (left_sp * 0.011358) / distance_in
         time.sleep(time_s)
         left_motor.stop()
         right_motor.stop(stop_action="brake")
