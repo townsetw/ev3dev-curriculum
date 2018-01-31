89eb5f110278e5a83815f5e17409ed5b6dcba816
townsetw <townsetw@rose-hulman.edu>
2018-01-31 14:52:07 -0500

    Working on drive motors

--- sandbox/m4/motors/m1_drive_timed.py
+++ sandbox/m4/motors/m1_drive_timed.py
@@ -91,6 +91,10 @@ def main():
     while time_s != 0:
         left_sp = int(input("Enter a speed (0 to 900 dps): "))
         distance_in = int(input("Enter a distance to travel (inches): "))
+        if left_sp == 0:
+            break
+        if distance_in == 0:
+            break
         left_motor.run_forever(speed_sp=left_sp)
         right_motor.run_forever(speed_sp=left_sp)
         time_s = distance_in / (left_sp * 0.011358)
@@ -113,14 +117,17 @@ main()
 # to:
 #   Enter a speed (0 to 900 dps):
 #   Distance to travel (inches):
-# TODO: 5. Write the code necessary to make the robot drive at that speed going roughly that distance.
+# DONE: 5. Write the code necessary to make the robot drive at that speed going
+#  roughly that distance.
 #   Note, in this module, you are REQUIRED to use the pattern...
 #      run_forever()
 #      time.sleep(some_amount)
 #      stop()
 #   You may NOT use the advanced motor commands at this time like: run_to_abs_pos, run_to_rel_pos, or run_timed.
-# TODO: 6. Modify the program so that it will exit immediately if the answer to   any   question is 0.
-# TODO: 7. Formally test your work. When you think you have the problem complete run these tests to be sure:
+# DONE: 6. Modify the program so that it will exit immediately if the answer to
+#    any   question is 0.
+# DONE: 7. Formally test your work. When you think you have the problem
+# complete run these tests to be sure:
 #   200 dps 24 inches (make sure it drives within 6 inches of the target distance)
 #   400 dps 24 inches (make sure it drives within 6 inches of the target distance)
 #   800 dps 24 inches (make sure it drives within 6 inches of the target distance)
@@ -128,6 +135,7 @@ main()
 #   400 dps 36 inches (make sure it drives within 9 inches of the target distance)
 # Do more tests if you see fit.  Ideally you should be +/- 25% of the target goal.
 #
-# TODO: 8. Call over a TA or instructor to sign your team's checkoff sheet and do a code review.
+# DONE: 8. Call over a TA or instructor to sign your team's checkoff sheet and
+# do a code review.
 #
 #  Observation you should make, the pattern run_forever-->time.sleep-->stop naturally blocks code execution until done.
--- sandbox/m4/motors/m2_drive_encoders.py
+++ sandbox/m4/motors/m2_drive_encoders.py
@@ -4,13 +4,57 @@ This module lets you practice using the encoder to determine distances while blo
 
 You will now use a run_to_rel_pos command to implement the action drive inches action.
 
-Authors: David Fisher and PUT_YOUR_NAME_HERE.
-"""  # TODO: 1. PUT YOUR NAME IN THE ABOVE LINE.
+Authors: David Fisher and TYLER, NATHAN, DREW.
+"""  # DONE: 1. PUT YOUR NAME IN THE ABOVE LINE.
 
-# TODO: 2. Copy the contents of your m1_drive_timed.py and paste that text into this file below these comments.
+# DONE: 2. Copy the contents of your m1_drive_timed.py and paste that text into
+#  this file below these comments.
 #   If your program says and prints anything at the start change it to print and say "Drive using encoders"
 
-# TODO: 3. Add a beep after the drive motors stop (see code below).  Test your code to hear the beep AFTER movement.
+import ev3dev.ev3 as ev3
+import time
+
+
+def main():
+    print("--------------------------------------------")
+    print("  Drive using input")
+    print("--------------------------------------------")
+    ev3.Sound.speak("Drive using input").wait()
+
+    # Connect two large motors on output ports B and C
+    left_motor = ev3.LargeMotor(ev3.OUTPUT_B)
+    right_motor = ev3.LargeMotor(ev3.OUTPUT_C)
+
+    # Check that the motors are actually connected
+    assert left_motor.connected
+    assert right_motor.connected
+
+    time_s = 1  # Any value other than 0.
+    while time_s != 0:
+        left_sp = int(input("Enter a speed (0 to 900 dps): "))
+        distance_in = int(input("Enter a distance to travel (inches): "))
+        if left_sp == 0:
+            break
+        if distance_in == 0:
+            break
+        left_motor.run_forever(speed_sp=left_sp)
+        right_motor.run_forever(speed_sp=left_sp)
+        time_s = distance_in / (left_sp * 0.011358)
+        time.sleep(time_s)
+        left_motor.stop()
+        right_motor.stop(stop_action="brake")
+        ev3.Sound.beep().wait()
+
+    print("Goodbye!")
+    ev3.Sound.speak("Goodbye").wait()
+
+# ----------------------------------------------------------------------
+# Calls  main  to start the ball rolling.
+# ----------------------------------------------------------------------
+main()
+
+# DONE: 3. Add a beep after the drive motors stop (see code below).  Test your
+# code to hear the beep AFTER movement.
 #   ev3.Sound.beep().wait()
 
 # TODO: 4. Instead of using the run_forever, time.sleep, stop pattern switch to using the run_to_rel_pos command.
