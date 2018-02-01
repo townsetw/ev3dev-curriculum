5646e7f653c091faee98bf948271d7b1380e9711
townsetw <townsetw@rose-hulman.edu>
2018-02-01 13:41:31 -0500

    Working on drive motors

--- sandbox/m4/motors/m4_turn_degrees.py
+++ sandbox/m4/motors/m4_turn_degrees.py
@@ -6,7 +6,8 @@ Much like you have a drive_inches command in your library, you will now make a t
 Authors: David Fisher and TYLER, NATHAN, DREW.
 """  # DONE: 1. PUT YOUR NAME IN THE ABOVE LINE.
 
-# TODO: 2. Copy the contents of m3_drive_inches_via_library.py and paste that text into this file below these comments.
+# DONE: 2. Copy the contents of m3_drive_inches_via_library.py and paste that
+# text into this file below these comments.
 #   Change the initial print and speak commands to reflect this module, like this...
 #     print("--------------------------------------------")
 #     print(" Turn degrees")
@@ -26,24 +27,26 @@ def main():
     # You are not allowed to make any changes to this code.
     # --------------------------------------------------------------
     print("--------------------------------------------")
-    print(" Drive inches")
+    print(" Turn degrees")
     print("--------------------------------------------")
-    ev3.Sound.speak("Drive inches").wait()
+    ev3.Sound.speak("Turn degrees bro").wait()
+
     robot = robo.Snatch3r()
 
     while True:
         speed_deg_per_second = int(input("Speed (0 to 900 dps): "))
         if speed_deg_per_second == 0:
             break
-        inches_target = int(input("Distance (inches): "))
-        if inches_target == 0:
+        degrees_to_turn = int(input("Degrees To Turn (degrees): "))
+        if degrees_to_turn == 0:
             break
 
-        robot.drive_inches(inches_target, speed_deg_per_second)
+        robot.turn_degrees(degrees_to_turn,speed_deg_per_second)
+
         ev3.Sound.beep().wait()  # Fun little beep
 
     print("Goodbye!")
-    ev3.Sound.speak("Goodbye").wait()
+    ev3.Sound.speak("done son").wait()
 
 
 # ----------------------------------------------------------------------
@@ -51,7 +54,8 @@ def main():
 # ----------------------------------------------------------------------
 main()
 
-# TODO: 3. Create a method in your library called turn_degrees that receives the degrees_to_turn and turn_speed_sp
+# DONE: 3. Create a method in your library called turn_degrees that receives
+# the degrees_to_turn and turn_speed_sp
 #   To help you get started here is a potential method signature line that will be in your library.
 #
 #   def turn_degrees(self, degrees_to_turn, turn_speed_sp):
@@ -66,7 +70,8 @@ main()
 #     Use the run_to_rel_pos, .wait_while(ev3.Motor.STATE_RUNNING) pattern to implement your work (not timed driving)
 #   You will have to experimentally determine the formula for accurate position_sp turn amounts.
 
-# TODO: 4. Individually implement the code here to use your turn_degrees library method.
+# DONE: 4. Individually implement the code here to use your turn_degrees
+# library method.
 #   Modify the code to ask the user how many degrees they would like to turn
 #   Ask the user what speed they would like to use for the turn (0 to 900 degrees per second).
 #   Beep after the turn is complete via a beep in this module (tests to make sure the library is blocking)
@@ -82,6 +87,7 @@ main()
 #    360 degrees
 # Add more tests as you see fit.  Ideally you should be within 45 degrees of back where you started (if you can).
 
-# TODO: 6. Call over a TA or instructor to sign your team's checkoff sheet and do a code review.
+# DONE: 6. Call over a TA or instructor to sign your team's checkoff sheet and
+# do a code review.
 #
 # Observations you should make, using run_to_rel_pos is useful for accurate turns, but testing takes time.
