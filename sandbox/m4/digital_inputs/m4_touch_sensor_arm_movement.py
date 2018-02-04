5ebb3eefc20af43b95b36f3c3b04dc823a9d1c37
pryorna <pryorna@rose-hulman.edu>
2018-02-04 16:43:05 -0500

    button states

--- sandbox/m4/digital_inputs/m4_touch_sensor_arm_movement.py
+++ sandbox/m4/digital_inputs/m4_touch_sensor_arm_movement.py
@@ -106,7 +106,7 @@ def arm_up(arm_motor, touch_sensor):
 
     # Code that attempts to do this task but has many bugs.  Fix them!
     arm_motor.run_forever(speed_sp=MAX_SPEED)
-    while not touch_sensor.is_pressed:
+    while touch_sensor.is_pressed:
         time.sleep(0.01)
     arm_motor.stop(stop_action="brake")
     ev3.Sound.beep()
