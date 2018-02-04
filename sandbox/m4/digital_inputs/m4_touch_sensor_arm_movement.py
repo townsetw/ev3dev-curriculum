914a43a15805746ace5a28da1d978d15c9cc8b9c
mcintoap <mcintoap@rose-hulman.edu>
2018-02-04 16:44:01 -0500

    working

--- sandbox/m4/digital_inputs/m4_touch_sensor_arm_movement.py
+++ sandbox/m4/digital_inputs/m4_touch_sensor_arm_movement.py
@@ -2,16 +2,15 @@
 """
 In this module you will use the touch sensor to make arm movements.  Instead of writing code from scratch you will
 fix the existing code, which is FULL of bugs.
-
-Authors: David Fisher and Nathan Pryor, Drew McIntosh, Tyler Townsend.
-"""  # DONE: 1. PUT YOUR NAME IN THE ABOVE LINE.
+Authors: David Fisher and Group 27.
+"""  # done: 1. PUT YOUR NAME IN THE ABOVE LINE.
 
 import ev3dev.ev3 as ev3
 import time
 
 MAX_SPEED = 900
 
-# DONE: 2. Have someone on your team run this program as is on the EV3 and make
+# done: 2. Have someone on your team run this program as is on the EV3 and make
 #  sure everyone understands the code.
 # Can you see what the robot does and explain what each line of code is doing? Talk as a group to make sure.
 
@@ -32,15 +31,18 @@ def main():
         command_to_run = input("Enter c (for calibrate), u (for up), d (for down), or q (for quit): ")
         if command_to_run == 'c':
             print("Calibrate the arm")
-
+            #print("to do: 3 is to delete this print statement, uncomment the
+            # line below, and implement that function.")
             arm_calibration(arm_motor, touch_sensor)
         elif command_to_run == 'u':
             print("Move the arm to the up position")
-
+            #print("TO DO: 4 is to delete this print statement, uncomment
+            # the line below, and implement that function.")
             arm_up(arm_motor, touch_sensor)
         elif command_to_run == 'd':
             print("Move the arm to the down position")
-
+            #print("TO DO: 5 is to delete this print statement, uncomment
+            # the line below, and implement that function.")
             arm_down(arm_motor)
         elif command_to_run == 'q':
             break
@@ -56,12 +58,11 @@ def arm_calibration(arm_motor, touch_sensor):
     Runs the arm up until the touch sensor is hit then back to the bottom again, beeping at both locations.
     Once back at in the bottom position, gripper open, set the absolute encoder position to 0.  You are calibrated!
     The Snatch3r arm needs to move 14.2 revolutions to travel from the touch sensor to the open position.
-
     Type hints:
       :type arm_motor: ev3.MediumMotor
       :type touch_sensor: ev3.TouchSensor
     """
-    # DONE: 3. Implement the arm calibration movement by fixing the code below
+    # done: 3. Implement the arm calibration movement by fixing the code below
     # (it has many bugs).  It should to this:
     #   Command the arm_motor to run forever in the positive direction at max speed.
     #   Create an infinite while loop that will block code execution until the touch sensor's is_pressed value is True.
@@ -75,29 +76,32 @@ def arm_calibration(arm_motor, touch_sensor):
     #   Set the arm encoder position to 0 (the last line below is correct to do that, it's new so no bug there)
 
     # Code that attempts to do this task but has MANY bugs (nearly 1 on every line).  Fix them!
-    arm_motor.run_forever(speed_sp=MAX_SPEED)
+
+    arm_motor.run_forever(speed_sp = MAX_SPEED)
+
     while not touch_sensor.is_pressed:
         time.sleep(0.01)
-    arm_motor.stop(stop_action="break")
 
-    arm_revolutions_for_full_range = 14.2*360
+    arm_motor.stop(stop_action="brake")
+    ev3.Sound.beep()
+
+    arm_revolutions_for_full_range = 14.2 * 360
     arm_motor.run_to_rel_pos(position_sp=-arm_revolutions_for_full_range)
     arm_motor.wait_while(ev3.Motor.STATE_RUNNING)
     ev3.Sound.beep()
 
-
-    arm_motor.position = 0  # Calibrate the down position as 0 (this line is correct as is).
+    arm_motor.position = 0  # Calibrate the down position as 0 (this line is
+    # correct as is).
 
 
 def arm_up(arm_motor, touch_sensor):
     """
     Moves the Snatch3r arm to the up position.
-
     Type hints:
       :type arm_motor: ev3.MediumMotor
       :type touch_sensor: ev3.TouchSensor
     """
-    # TODO: 4. Implement the arm up movement by fixing the code below
+    # done: 4. Implement the arm up movement by fixing the code below
     # Command the arm_motor to run forever in the positive direction at max speed.
     # Create a while loop that will block code execution until the touch sensor is pressed.
     #   Within the loop sleep for 0.01 to avoid running code too fast.
@@ -106,7 +110,7 @@ def arm_up(arm_motor, touch_sensor):
 
     # Code that attempts to do this task but has many bugs.  Fix them!
     arm_motor.run_forever(speed_sp=MAX_SPEED)
-    while touch_sensor.is_pressed:
+    while not touch_sensor.is_pressed:
         time.sleep(0.01)
     arm_motor.stop(stop_action="brake")
     ev3.Sound.beep()
@@ -115,16 +119,16 @@ def arm_up(arm_motor, touch_sensor):
 def arm_down(arm_motor):
     """
     Moves the Snatch3r arm to the down position.
-
     Type hints:
       :type arm_motor: ev3.MediumMotor
     """
-    # TODO: 5. Implement the arm up movement by fixing the code below
+    # done: 5. Implement the arm up movement by fixing the code below
     # Move the arm to the absolute position_sp of 0 at max speed.
     # Wait until the move completes
     # Make a beep sound
 
     # Code that attempts to do this task but has bugs.  Fix them.
+
     arm_motor.run_to_abs_pos(position_sp=0, speed_sp=MAX_SPEED)
     arm_motor.wait_while(ev3.Motor.STATE_RUNNING)  # Blocks until the motor finishes running
     ev3.Sound.beep()
