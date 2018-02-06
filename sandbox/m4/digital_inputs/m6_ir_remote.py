4f6ba19c7ee04a33209c7f25ff01f97cdac9b934
mcintoap <mcintoap@rose-hulman.edu>
2018-02-06 11:45:41 -0500

    DONE WITH M6

--- sandbox/m4/digital_inputs/m6_ir_remote.py
+++ sandbox/m4/digital_inputs/m6_ir_remote.py
@@ -1,9 +1,7 @@
 #!/usr/bin/env python3
 """
   This is your opportunity to work as a team on a mini-project.  You will need to handle the following actions.
-
   When your program starts it should say IR Remote, print instructions, and do a robot.arm_calibration()
-
   IR remote channel 1 to drive the crawler tracks around:
     -- Pressing  red up   makes the left  LED turn green and the left_motor  move at  600.
          Releasing turns off the LED and stops left_motor.
@@ -15,28 +13,24 @@
          Releasing turns off the LED and stops right_motor.
     You should be able to use one red (left) button and one blue (right) button at the same time.  For example:
      while pressing both red up and blue down the robot should spin and LEDs will be green (left) and red (right).
-
   IR remote channel 2 to raise and lower the arm
     -- Pressing red up   calls robot.arm_up().
     -- Pressing red down calls robot.arm_down().
     -- Pressing blue up  calls robot.arm_calibration().
-
   Buttons
     -- Pressing the Back button will allow your program to end.  It should stop motors, turn on both green LEDs, and
        then print and say Goodbye.  You will need to implement a new robot method called shutdown to handle this task.
-
-Authors: David Fisher and Robot 27.
-"""  # Done: 1. PUT YOUR NAME IN THE ABOVE LINE.
-
-import time
+Authors: David Fisher and Group 24.
+"""  # done: 1. PUT YOUR NAME IN THE ABOVE LINE.
 
 import ev3dev.ev3 as ev3
+import time
 
 import robot_controller as robo
 
-
 # Note that todo2 is farther down in the code.  That method needs to be written before you do todo3.
-# TODO: 3. Have someone on your team run this program on the EV3 and make sure everyone understands the code.
+# done: 3. Have someone on your team run this program on the EV3 and make sure
+# everyone understands the code.
 # Can you see what the robot does and explain what each line of code is doing? Talk as a group to make sure.
 
 
@@ -60,6 +54,14 @@ def main():
     robot = robo.Snatch3r()
     dc = DataContainer()
 
+    # Connect two large motors on output ports B and C
+    left_motor = ev3.LargeMotor(ev3.OUTPUT_B)
+    right_motor = ev3.LargeMotor(ev3.OUTPUT_C)
+
+    # Check that the motors are actually connected
+    assert left_motor.connected
+    assert right_motor.connected
+
     # TODO: 4. Add the necessary IR handler callbacks as per the instructions above.
     # Remote control channel 1 is for driving the crawler tracks around (none of these functions exist yet below).
     # Remote control channel 2 is for moving the arm up and down (all of these functions already exist below).
@@ -67,10 +69,10 @@ def main():
     rc1 = ev3.RemoteControl(channel=1)
     assert rc1.connected
 
-    rc1.on_red_up = lambda state: handle_red_up_1(state, )
-    rc1.on_red_down = lambda state: handle_red_down_1(state, )
-    rc1.on_blue_up = lambda state: handle_blue_up_1(state, )
-    rc1.on_blue_down = lambda state: handle_blue_down_1(state, )
+    rc1.on_red_up = lambda state: handle_red_up(state, left_motor)
+    rc1.on_red_down = lambda state: handle_red_up(state, left_motor)
+    rc1.on_blue_up = lambda state: handle_blue_up(state, right_motor)
+    rc1.on_blue_down = lambda state: handle_blue_down(state, right_motor)
 
     rc2 = ev3.RemoteControl(channel=2)
     assert rc2.connected
@@ -86,17 +88,20 @@ def main():
     robot.arm_calibration()  # Start with an arm calibration in this program.
 
     while dc.running:
-        # TODO: 5. Process the RemoteControl objects.
+        # done: 5. Process the RemoteControl objects.
+        rc1.process()
+        rc2.process()
         btn.process()
         time.sleep(0.01)
 
-    # DONE: 2. Have everyone talk about this problem together then pick one
+    # done: 2. Have everyone talk about this problem together then pick one
     # member to modify libs/robot_controller.py
     # as necessary to implement the method below as per the instructions in the opening doc string. Once the code has
     # been tested and shown to work, then have that person commit their work.  All other team members need to do a
     # VCS --> Update project...
     # Once the library is implemented any team member should be able to run his code as stated in todo3.
     robot.shutdown()
+
 
 # ----------------------------------------------------------------------
 # Event handlers
@@ -105,9 +110,52 @@ def main():
 # ----------------------------------------------------------------------
 # TODO: 6. Implement the IR handler callbacks handlers.
 
-def handle_red_up_1(button_state, robot):
+# ----------------------------------------------------------------------
+# IR Remote callbacks
+# ----------------------------------------------------------------------
+def handle_red_up(button_state, left_motor):
+
     if button_state:
         ev3.Leds.set_color(ev3.Leds.LEFT, ev3.Leds.GREEN)
+        left_motor.run_forever(speed_sp=600)
+    else:
+        ev3.Leds.set_color(ev3.Leds.LEFT, ev3.Leds.BLACK)
+        left_motor.stop(stop_action="brake")
+
+
+def handle_red_down(button_state, left_motor):
+
+    if button_state:
+        ev3.Leds.set_color(ev3.Leds.LEFT, ev3.Leds.RED)
+        left_motor.run_forever(speed_sp=-600)
+    else:
+        ev3.Leds.set_color(ev3.Leds.LEFT, ev3.Leds.BLACK)
+        left_motor.stop(stop_action="brake")
+
+
+def handle_blue_up(button_state, right_motor):
+
+    if button_state:
+        ev3.Leds.set_color(ev3.Leds.RIGHT, ev3.Leds.RED)
+        right_motor.run_forever(speed_sp=600)
+    else:
+        ev3.Leds.set_color(ev3.Leds.RIGHT, ev3.Leds.BLACK)
+        right_motor.stop(stop_action="brake")
+
+
+
+def handle_blue_down(button_state, right_motor):
+
+    if button_state:
+        ev3.Leds.set_color(ev3.Leds.RIGHT, ev3.Leds.RED)
+        right_motor.run_forever(speed_sp=-600)
+    else:
+        ev3.Leds.set_color(ev3.Leds.RIGHT, ev3.Leds.BLACK)
+        right_motor.stop(stop_action="brake")
+
+
+
+
 
 # TODO: 7. When your program is complete, call over a TA or instructor to sign your checkoff sheet and do a code review.
 #
@@ -117,7 +165,6 @@ def handle_red_up_1(button_state, robot):
 def handle_arm_up_button(button_state, robot):
     """
     Moves the arm up when the button is pressed.
-
     Type hints:
       :type button_state: bool
       :type robot: robo.Snatch3r
@@ -129,7 +176,6 @@ def handle_arm_up_button(button_state, robot):
 def handle_arm_down_button(button_state, robot):
     """
     Moves the arm down when the button is pressed.
-
     Type hints:
       :type button_state: bool
       :type robot: robo.Snatch3r
@@ -141,7 +187,6 @@ def handle_arm_down_button(button_state, robot):
 def handle_calibrate_button(button_state, robot):
     """
     Has the arm go up then down to fix the starting position.
-
     Type hints:
       :type button_state: bool
       :type robot: robo.Snatch3r
@@ -153,7 +198,6 @@ def handle_calibrate_button(button_state, robot):
 def handle_shutdown(button_state, dc):
     """
     Exit the program.
-
     Type hints:
       :type button_state: bool
       :type dc: DataContainer
