912c4fee7f657f8dc9d1a4aac7298faef91c4768
townsetw <townsetw@rose-hulman.edu>
2018-02-07 14:55:33 -0500

    asdf

--- sandbox/m4/mqtt/m1_pc_shared_circles.py
+++ sandbox/m4/mqtt/m1_pc_shared_circles.py
@@ -108,7 +108,7 @@ def left_mouse_click(event, mqtt_client):
     # Repeated: If you uncommented the code above to test it, make sure to comment it back out before todo7 below.
 
     # MQTT draw
-    # TODO: 7. Send a message using MQTT that will:
+    # DONE: 7. Send a message using MQTT that will:
     #   - Call the method called "on_circle_draw" on the delegate at the other end of the pipe.
     #   - Pass the parameters [my_color, event.x, event.y] as a list.
     # This is the only TO DO you have to think about.  It is meant to help you learn the mqtt_client.send_message syntax
@@ -118,11 +118,12 @@ def left_mouse_click(event, mqtt_client):
 
     mqtt_client.send_message("on_circle_draw", [my_color,event.x, event.y])
 
-    # TODO: 8. Help get everyone on your team running this program at the same time.
+    # DONE: 8. Help get everyone on your team running this program at the same
+    # time.
     # You should be able to see circles on your computer from everyone else on your team.
     # Try to draw the first letter of your name in circles. :)
 
-    # TODO: 9. Call over a TA or instructor to sign your team's checkoff sheet.
+    # DONE: 9. Call over a TA or instructor to sign your team's checkoff sheet.
     #
     # Observations you should make, with MQTT your team can hear your messages.
     # You published messages to the "legoXX/draw" topic (where XX is the number set in libs/mqtt_remote_method_calls.py)
