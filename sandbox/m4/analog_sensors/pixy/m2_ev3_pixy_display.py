4cd3dd66a767e6725992b73f3fc7c1633ea85805
mcintoap <mcintoap@rose-hulman.edu>
2018-02-12 03:24:21 -0500

    Analog sensores

--- sandbox/m4/analog_sensors/pixy/m2_ev3_pixy_display.py
+++ sandbox/m4/analog_sensors/pixy/m2_ev3_pixy_display.py
@@ -2,15 +2,15 @@
 """
 The goal of this module is to practice using the Pixy and MQTT at the same time.  This module will send data from the
 EV3 to the PC.
-
-Authors: David Fisher and PUT_YOUR_NAME_HERE.  February 2017.
-"""  # TODO: 1. PUT YOUR NAME IN THE ABOVE LINE.
-
-import time
+Authors: David Fisher and Lego 27.
+  February 2017.
+"""  # DONE: 1. PUT YOUR NAME IN THE ABOVE LINE.
 
 import ev3dev.ev3 as ev3
+import time
 
 import robot_controller as robo
+import mqtt_remote_method_calls as com
 
 
 def main():
@@ -21,22 +21,31 @@ def main():
     ev3.Sound.speak("Pixy display").wait()
     print("Press the touch sensor to exit this program.")
 
-    # TODO: 2. Create an MqttClient (no delegate needed since EV3 will only send data, so an empty constructor is fine)
+    # done: 2. Create an MqttClient (no delegate needed since EV3 will only
+    # send data, so an empty constructor is fine)
     # Then connect to the pc using the connect_to_pc method.
+
+    mqtt_client = com.MqttClient()
+    mqtt_client.connect_to_pc()
 
     robot = robo.Snatch3r()
     robot.pixy.mode = "SIG1"
 
     while not robot.touch_sensor.is_pressed:
 
-        # TODO: 3. Read the Pixy values for x, y, width, and height
+        # DONE: 3. Read the Pixy values for x, y, width, and height
         # Print the values (much like the print_pixy_readings example)
 
-        # TODO: 4. Send the Pixy values to the PC by calling the on_rectangle_update method
+        print("(X, Y) = ({}, {})    Width = {} Height = {}".format(
+            robot.pixy.value(1), robot.pixy.value(2), robot.pixy.value(3),
+            robot.pixy.value(4)))
+
+        # DONE: 4. Send the Pixy values to the PC by calling the
+        # on_rectangle_update method
         # If you open m2_pc_pixy_display you can see the parameters for that method [x, y, width, height]
 
-
-
+        mqtt_client.send_message("on_rectangle_update", [robot.pixy.value(
+            1), robot.pixy.value(2), robot.pixy.value(3), robot.pixy.value(4)])
 
         time.sleep(0.25)
 
@@ -44,7 +53,7 @@ def main():
     ev3.Sound.speak("Goodbye").wait()
     mqtt_client.close()
 
-# TODO: 5. Call over a TA or instructor to sign your team's checkoff sheet.
+# DONE: 5. Call over a TA or instructor to sign your team's checkoff sheet.
 #
 # Observations you should make, if the EV3 has data the PC can know that data too using MQTT.
 
