d8f2988f24d0f741602206e693c57bfd56f5a9eb
mcintoap <mcintoap@rose-hulman.edu>
2018-02-18 16:55:38 -0500

    Working on project

--- projects/mcintoap/ev3_project.py
+++ projects/mcintoap/ev3_project.py
@@ -1,5 +1,3 @@
-
-
 import mqtt_remote_method_calls as com
 import robot_controller as robo
 import ev3dev.ev3 as ev3
@@ -9,6 +7,7 @@ def main():
     robot = robo.Snatch3r()
     mqtt_client = com.MqttClient(robot)
     mqtt_client.connect_to_pc()
+
     ev3.Sound.speak("Maze Runner 2.0 ").wait()
     robot.arm_calibration()
     robot.loop_forever()
